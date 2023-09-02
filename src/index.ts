a52fd823c8a47b325417d3eec46b17c97504800a
khairalla17 <mkhairal@e3r7p17.1337.ma>
2023-09-02 03:35:13 +0100

    fixing restart backend

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ const port = Number(env.PORT || 8080)
 app.use(morgan('dev'))
 
 app.get("/", (req, res) => {
-    res.send("API DE MERDE!!")
+    debugger;
+    res.send("new api working and not!!")
 })
 
 app.listen(port, '0.0.0.0', () => console.log(`APP LISTENING ON PORT http://localhost:${port}`))
